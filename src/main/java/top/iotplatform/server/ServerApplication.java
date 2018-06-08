71d91e0cd122dc9d7de448c2b262a61e5ead68fc
oliujunk <413041277@qq.com>
2018-06-08 08:33:23 +0800

    update

--- src/main/java/top/iotplatform/server/ServerApplication.java
+++ src/main/java/top/iotplatform/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package top.iotpaltform.server;
+package top.iotplatform.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
